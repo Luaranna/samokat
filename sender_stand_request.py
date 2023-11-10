6d4ad690cf92b8bcc3ad025534960380b7a798b8
Luaranna <luaranna@gmail.com>
2023-11-10 20:31:17 +0000

    update code

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,13 +4,11 @@ import requests
 
 
 # Функция для создания нового заказа
-def post_new_order_track(body):
+def post_new_order_track():
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
-                         json=body)
+                         json=data.order_body)
 
 
 # Функция для получения заказа по номеру заказа
-def get_order_by_track():
-    response = post_new_order_track(data.order_body)
-    new_order_track = str (response.json()['track'])
-    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + new_order_track)
+def get_order_by_track(track):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + str(track))
